4f133642642ec96eef43098a14925379d587c5ba
WiktoriaGit <wiktoriagithub@gmail.com>
2024-11-17 19:11:21 +0100

    Dokumentacja MergeSort.cpp

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include "MergeSort.h"
 
-
+/**
+* Ustawia MergeSort::size na ```0``` oraz tworzy pust¹ tablicê
+*/
 MergeSort::MergeSort()
 {
     this->size = 0;
     this->arr = new int[this->size];
 }
 
+/**
+* Ustawia MergeSort::size na wartoœæ przekazan¹ przez parametr ```size``` oraz
+* alokuje pamiêæ dla tablicy MergeSort::arr o rozmiarze ```size```
+*
+* Inicjalizuje tablicê funkcj¹ MergeSort::setArray()
+*
+* Sortuje tablicê wywo³uj¹c funkcjê MergeSort::sort()
+*/
 MergeSort::MergeSort(int input[], int size)
 {
     this->size = size;
@@ -18,11 +28,17 @@ MergeSort::MergeSort(int input[], int size)
     this->sort();
 }
 
+/**
+* Destruktor wywo³uje ```delete``` na tablicy, zwalniaj¹c pamiêæ.
+*/
 MergeSort::~MergeSort()
 {
     delete[] arr;
 }
 
+/**
+* Printowanie na ekran poszczególnych elementów tablicy za pomoc¹ pêtli ```for``` oraz ```std::cout```
+*/
 void MergeSort::printArray()
 {
     for (int i = 0; i < this->size; i++) {
@@ -31,6 +47,9 @@ void MergeSort::printArray()
     std::cout << std::endl;
 }
 
+/**
+* Funkcja inicjalizuje tablicê, wype³niaj¹c j¹ indeksami pêtli ```for```
+*/
 void MergeSort::setArray(int input[], int inputSize)
 {
     for (int i = 0; i < inputSize; i++) {
@@ -38,6 +57,11 @@ void MergeSort::setArray(int input[], int inputSize)
     }
 }
 
+/**
+* Inicjalizuje proces sortowania tablicy wywo³uj¹c rekurencyjnie MergeSort::mergeSort.
+*
+* Funkcja zakoñczy siê gdy ```size``` bêdzie ```<= 1```
+*/
 void MergeSort::sort()
 {
     if (size <= 1) 
@@ -46,11 +70,19 @@ void MergeSort::sort()
     mergeSort(arr, 0, size - 1);
 }
 
+/**
+* Zwrócenie wskaŸnika ```*arr```
+*/
 int* MergeSort::getArray()
 {
     return this->arr;
 }
 
+/**
+* Wywo³ywana rekurencj¹. Wyznacza œrodek tablicy i przekazuje jako parametr granice do podzia³u na mniejsze tablice.
+*
+* Gdy tablice bêd¹ wystarczaj¹co ma³e, nastêpuje wywo³anie MergeSort::merge() aby posortowaæ i scaliæ tablice.
+*/
 void MergeSort::mergeSort(int* arr, int left, int right)
 {
     if (left < right) {
@@ -64,6 +96,17 @@ void MergeSort::mergeSort(int* arr, int left, int right)
     }
 }
 
+/**
+* Wyznaczane s¹ wielkoœci ```n1``` oraz ```n2``` dla nowych tablic ```leftArr``` oraz ```rightArr``` oraz alokacja pamiêci.
+*
+* Dane s¹ kopiowane do nowych tablic ```leftArr``` oraz ```rightArr```.
+*
+* Nastêpnie s¹ tablice sortowane przez porównanie oraz scalane.
+*
+* Je¿eli pozosta³y jakiekolwiek elementy w tablicach, s¹ one tak¿e kopiowane.
+*
+* Na samym koñcu nastêpuje zwolnienie pamiêci dla dwóch tymczasowych tablic.
+*/
 void MergeSort::merge(int* arr, int left, int mid, int right)
 {
     int n1 = mid - left + 1;
